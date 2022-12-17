e7011d762fda0df032470e30d709a149794fb8a2
Justin <jwinstead120@gmail.com>
2022-12-17 11:14:06 -0800

    added all CRUD functionality

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -43,7 +43,90 @@ const tripsFindByCode = async (req, res) => {
         });
 };
 
+const tripsAddTrip = async (req, res) => {
+    Model
+        .create({
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description
+        },
+        (err, trip) => {
+            if (err) {
+                return res
+                    .status(400)
+                    .json(err);
+            }
+            else {
+                return res
+                    .status(201)
+                    .json(trip);
+            }
+        });
+}
+
+const tripsUpdateTrip = async (req, res) => {
+    console.log(req.body);
+    Model
+        .findOneAndUpdate({'code': req.params.tripCode}, {
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description
+        }, { new: true })
+        .then(trip => {
+            if (!trip) {
+            return res
+                .status(404)
+                .send({
+                    message: "Trip not found with code " + req.params.tripCode
+                    });
+            }
+            res.send(trip);
+        }).catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res
+                    .status(404)
+                    .send({
+                        message: "Trip not found with code " + req.params.tripCode
+                    });
+            }
+            return res
+                .status(500) // server error
+                .json(err);
+        });
+}
+
+
+const tripsDeleteTrip = async (req, res) => {
+    console.log("Inside tripsDeleteTrip");
+    Model 
+        .findOneAndRemove({'code': req.params.tripCode})
+        .exec((err, trip) =>  {
+            if (err) {
+                return res
+                .status(404)
+                .json(err);
+            }
+            res
+            .status(204)
+            .json(null);
+        });
+};
+       
+
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip,
+    tripsDeleteTrip
 }
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,10 +5,13 @@ const controller = require('../controllers/trips');
 /* GET homepage */
 router
     .route('/trips')
-    .get(controller.tripsList);
+    .get(controller.tripsList)
+    .post(controller.tripsAddTrip);
 
 router
     .route('/trips/:tripCode')
-    .get(controller.tripsFindByCode);
+    .get(controller.tripsFindByCode)
+    .put(controller.tripsUpdateTrip)
+    .delete(controller.tripsDeleteTrip);
 
 module.exports = router;
